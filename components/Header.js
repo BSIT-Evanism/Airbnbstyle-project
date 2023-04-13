a0224a4c3860b329c4d2ec77e5171cb9450afc23
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-04-13 23:23:27 +0800

    next commit for vercel hosting

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,42 @@
-import Image from "next/image"
+import Image from "next/image";
+import {GlobeAltIcon,
+        MagnifyingGlassIcon,
+        Bars3Icon,
+        UserCircleIcon} from "@heroicons/react/24/solid";
 
 function Header() {
-	return (
-			<header>
-				<div className="relative flex items-center h-10 cursor-pointer">
-					<Image 
-						src="https://links.papareact.com/qd3" 
-						layout="fill" 
-						objectFit="contain"
-						objectPosition="left"
-						/>
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+          alt="airbnb logo"
+        />
+      </div>
 
-					<div></div>
-				</div>
-		
-			</header>
-			)
+      <div className="flex items-center md:border-2 md:shadow-md rounded-full py-2">
+        <input
+          className="pl-5 flex-grow bg-transparent"
+          type="text"
+          placeholder="Start your search"
+        />
+        <MagnifyingGlassIcon className="hidden md:inline-flex md:mx-2 h-8 bg-red-400 text-white rounded-full p-2" />
+      </div>
+
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex items-center space-x-4 border-2 p-2 rounded-full">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,71 @@
 import Head from "next/head";
-import Header from "components/Header"
+import Header from "components/Header";
+import Banner from "components/Banner";
+import SmallCard from "@/components/SmallCard";
+import MediumCard from "@/components/MediumCard";
+import LargeCard from "@/components/LargeCard";
 
-export default function Home() {
+export default function Home({ exploreData, cardsData }) {
   return (
-      <div>
-        <Head>
-          <title>Airbnb style app</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <div>
+      <Head>
+        <title>Airbnb style app</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <h1>Hello World</h1>
-        <Header />
-      </div>
-      )
+      <Header />
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, location, distance }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Go Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3 ">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="All provided by Airbnb"
+          buttonText="Get Inspired"
+        />
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
 }
