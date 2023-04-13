e39c0011d01a5cecc15abcd413d22a28589c93a3
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-04-13 23:32:01 +0800

    few fixes

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -4,7 +4,12 @@ function SmallCard({ img, location, distance }) {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-200 hover:scale-105 transition transform duration-300 ease-in-out ">
       <div className="relative h-16 w-16">
-        <Image src={img} layout="fill" className="rounded-lg" />
+        <Image
+          src={img}
+          layout="fill"
+          className="rounded-lg"
+          alt="small images"
+        />
       </div>
 
       <div>
